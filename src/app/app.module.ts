bc4a45fa641fbd5b48146a8cead23ecc62ef95b7
NaveenDwivedi007 <naveen007.1994@gmail.com>
2021-07-06 15:08:09 +0530

    add component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './component/list/list.component';
+import { GanttComponent } from './component/gantt/gantt.component';
+import { BoardComponent } from './component/board/board.component';
+import { ViewComponent } from './component/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    GanttComponent,
+    BoardComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
